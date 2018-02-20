7426c168d96414359e9daec93fd944f4b8afd6e0
Bruno Tomé <ibrunotome@gmail.com>
2018-02-20 20:46:12 -0300

    Testando rotas autenticadas.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,15 +9,25 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     use DatabaseMigrations;
-    
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testHomeResponseStatus()
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testAuthentication()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get('/home');
 
         $response->assertStatus(200);
     }
